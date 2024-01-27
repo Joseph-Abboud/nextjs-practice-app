e95e082ae49c8a19ade95ef4ead6a0e1a1319100
Joseph Abboud <j.abboud1@hotmail.com>
2024-01-27 00:19:03 -0300

    Course by Mosh complete here is the link: https://www.youtube.com/watch?v=ZVnjOPwW4ZA&t=14s 100% Recommend

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AddToCart from "./AddToCart";
+import stlyes from "./ProductCard.module.css";
+
+const ProductCard = () => {
+  return (
+    <div className="">
+      <AddToCart />
+    </div>
+  );
+};
+
+export default ProductCard;
---
+++ app/users/page.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <table
+        className="table table-bordered
+    "
+      >
+        <thead>
+          <tr>
+            <td className="text-white">Name</td>
+            <td>Email</td>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default UsersPage;
